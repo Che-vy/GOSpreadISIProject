5f6978ae425b5c12f77082360ae3b6ac5b6b932d
Samuel Perreault <samuelperreault@me.com>
2018-09-13 14:27:41 -0400

    InputPackage done
    InputManager - Initial input recognition : GS-9
    
    Finished the getOnClickMouse function
    InputPackage finalized too
    Need to do test before review.

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -12,6 +12,8 @@ public class InputManager : MonoBehaviour
         inputPkg.currentMousePos = Input.mousePosition;
         float x = Input.GetAxis("Horizontal"); // get left right for the camera to move
         float z = Input.GetAxis("Vertical"); // get up down for the camera to move
+        inputPkg.cameraDirectionPressed = new Vector3(x, 0, z); //  **** la valeur y du vecteur a besoin de la hauteur de base (hauteur qui ne changera jamais ****
+        inputPkg.mousePosOnClick = getOnClickMousePosWithRayCast();
 
         return inputPkg;
     }
@@ -21,7 +23,7 @@ public class InputManager : MonoBehaviour
     {
         public Vector3 mousePosOnClick; // get position where the mouse clicked
         public Vector3 currentMousePos; // keep track of the mouse position (current position)
-        public Vector2 cameraDirectionPressed; // key pressed to move the camera around
+        public Vector3 cameraDirectionPressed; // key pressed to move the camera around
 
         public override string ToString()
         {
@@ -30,14 +32,18 @@ public class InputManager : MonoBehaviour
         }
     }
 
- //   public Vector3 getOnClickMousePosWithRayCast() // function to get mouse click position (need raycast to get the Z)
- //   {
- //       Vector3 mouse = Input.mousePosition;
- //       Ray castPoint = Camera.main.ScreenPointToRay(mouse);
- //       RaycastHit hit;
- //       if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
- //       {
- //           gameObject.transform.position = hit.point;
- //       }
- //   }
+   public Vector3 getOnClickMousePosWithRayCast() // function to get mouse click position (need raycast to get the Z)
+   {
+
+       Vector3 toRet = new Vector3(0,0,0);
+       Vector3 mouse = Input.mousePosition;
+       Ray castPoint = Camera.main.ScreenPointToRay(mouse); // transform 2D position of the mouse to 3D position for the mouse
+       RaycastHit hit;
+       if (Physics.Raycast(castPoint, out hit, Mathf.Infinity)) //if hit return true
+       {
+            Debug.Log("Raycast hit");
+            toRet = hit.point;
+       }
+        return toRet;
+   }
 }
