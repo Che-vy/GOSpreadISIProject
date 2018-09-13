39039bd9e727bab24d43f6b52691926481628a0b
Samuel Perreault <samuelperreault@me.com>
2018-09-13 12:51:43 -0400

    Fixed errors in the BasePawnClasses, created InputPkg
    InputManager - Initial input recognition :  GS9
    
    Added logic inside InputPkg :
    Get mouse pos (on screen)
    get x and z axis for the camera(move camera with up down left right arrows)
    
    Added fucntion to use raycast to get the mouse position in the 3D world. Function is not ready yet.
    :art:

--- Assets/Scripts/Pawns/Zips.cs
+++ Assets/Scripts/Pawns/Zips.cs
@@ -7,7 +7,7 @@ public class Zips : BasePawnsClass
 
     public Zips(Vector3 _pos)
     {
-        PawnTypes pawnType = PawnTypes.Zip;
+        pawnType = PawnTypes.Zip;
         quickPawnDefinition = "Upgraded form of the Bit. Can be upgraded into a Kernel if enough territories are possessed." +
                               "This unit can move 2 tiles per turn and have a range of one. " +
                               "It is also the only unit that can kills other unit by passing above an enemy unit like in checkers";
--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public InputPkg GetInputs()
+    {
+        InputPkg inputPkg = new InputPkg();
+
+        inputPkg.currentMousePos = Input.mousePosition;
+        float x = Input.GetAxis("Horizontal"); // get left right for the camera to move
+        float z = Input.GetAxis("Vertical"); // get up down for the camera to move
+
+        return inputPkg;
+    }
+
+
     public class InputPkg
     {
-        public Vector3 mousePosOnClick;
-        public Vector3 currentMousePos;
+        public Vector3 mousePosOnClick; // get position where the mouse clicked
+        public Vector3 currentMousePos; // keep track of the mouse position (current position)
+        public Vector2 cameraDirectionPressed; // key pressed to move the camera around
+
+        public override string ToString()
+        {
+            return "Mouse current position at : " + currentMousePos + "  / Mouse click position at : " + mousePosOnClick +
+                "ArrowKey pressed for camera (left/right,up,down, forward, backward): " + cameraDirectionPressed + ".  ";
+        }
+    }
+
+    public Vector3 getOnClickMousePosWithRayCast()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+        {
+            gameObject.transform.position = hit.point;
+        }
     }
 }
