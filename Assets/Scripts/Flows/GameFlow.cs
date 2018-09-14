bb4b450e3c666a135c39e714a803d4f550db9638
Samuel Perreault <samuelperreault@me.com>
2018-09-14 11:35:07 -0400

    Tested inputpkg and raycast
    InputManager - Initial input recognition : GS-9
    
    Tested all the input to be sure it was working,
    Tested the raycast hit so it returns the object that I clicked on
    This could be changed depending if we want to be able to click all objects or only particular ones.
    Removed the get on click mouse position since getting the object with the raycats is better

--- Assets/Scripts/Flows/GameFlow.cs
+++ Assets/Scripts/Flows/GameFlow.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameFlow : MonoBehaviour {
+    InputManager inputManager;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        inputManager = new InputManager();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        inputManager.GetInputs();
+
+    }
 }
