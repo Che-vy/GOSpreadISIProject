0dc190e4b553153a7060c7177e3f16a87a64f801
Jeff-63 <40066325+Jeff-63@users.noreply.github.com>
2018-09-18 11:01:32 -0400

    Kernel UI
    Kernel UI : GS-15
    Creation of the kernel UI frame.
    Add Create A Bit button on the UI.

--- Assets/Scripts/Flows/GameFlow.cs
+++ Assets/Scripts/Flows/GameFlow.cs
@@ -16,13 +16,8 @@ public class GameFlow : Flow {
         //TerritoryGridManager.Instance.initialization();
         UnitFactory.Instance.Initialize();
         //UnitGridManager.Instance.initialization();
-<<<<<<< HEAD
-        //UnitGridManager.Instance.Initialize();
-        //UnitGridManager.Instance.AddUnit(10,10,1, UnitType.Bit);
-=======
         UnitGrid.Instance.Initialize();
         Demo.Instance.StartDemo(); //FOR DEMONSTRATION PURPOSES ONLY
->>>>>>> 4f540c99bf8d87fd42f4a96f782f4c00a370d67f
     }
 
     override
